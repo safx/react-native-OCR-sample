8e1227d30b23d00a1e6974ca1c2fb076c366af7f
Safx <safxdev@gmail.com>
2016-06-06 23:53:02 +0900

    add camera and fs

--- android/app/src/main/java/com/rnocrsample/MainActivity.java
+++ android/app/src/main/java/com/rnocrsample/MainActivity.java
@@ -1,6 +1,8 @@
 package com.rnocrsample;
 
 import com.facebook.react.ReactActivity;
+import com.rnfs.RNFSPackage;
+import com.lwansbrough.RCTCamera.RCTCameraPackage;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 
@@ -34,7 +36,9 @@ public class MainActivity extends ReactActivity {
     @Override
     protected List<ReactPackage> getPackages() {
         return Arrays.<ReactPackage>asList(
-            new MainReactPackage()
+            new MainReactPackage(),
+            new RNFSPackage(),
+            new RCTCameraPackage()
         );
     }
 }
